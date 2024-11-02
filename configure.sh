fdbb5c1eb20b7e8329319cb22d8d08964a6284f9
TokiZeng <gn02322440@gmail.com>
2024-11-02 17:28:13 +0800

    first commit

--- configure.sh
+++ configure.sh
@@ -3,5 +3,5 @@
 extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -fslp-vectorize -fvectorize"
 
 ./configure CXXFLAGS="--gcc-toolchain=/usr -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mcpu=cortex-a53" \
-CFLAGS="--gcc-toolchain=/usr -finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mcpu=cortex-a53 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize $extracflags" \
+CFLAGS="--gcc-toolchain=/usr -finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mcpu=cortex-a78 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize $extracflags" \
 CXX=clang++ CC=clang LDFLAGS="-v -flto -Wl,-hugetlbfs-align"
